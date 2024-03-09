f55fffe336cb8c248824d122c1594e5ffd81d227
aristidei <a.isingizwe@alustudent.com>
2024-03-10 01:01:59 +0200

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,28 @@
-function App() {
-  return <h1>Chit Chat</h1>;
-}
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import NotFound from "./pages/NotFoundPage";
+import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
-export default App;
+export default function App() {
+  const route = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route index element={<MainPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="signup" element={<SignupPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    )
+  );
+  return (
+    <div>
+      <RouterProvider router={route} />
+    </div>
+  );
+}
---
+++ frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+  return (
+    <article className="bg-mainYellow h-screen w-full flex flex-col items-center gap-8 text-center px-6">
+      <img
+        src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg"
+        alt="404"
+      />
+      <span className="font-bold text-5xl md:text-4xl sm:text-3xl">
+        404 PAGE
+      </span>
+      <p className="">The page you were looking for could not be found</p>
+      <Link
+        className="font-bold bg-black text-white px-10 py-2 rounded-full z-30"
+        to="/"
+      >
+        Go Back Home
+      </Link>
+    </article>
+  );
+}
