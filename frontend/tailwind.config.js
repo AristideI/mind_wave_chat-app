b29a321388c64f9cae9a8a49319674ec37aaeb87
aristidei <a.isingizwe@alustudent.com>
2024-03-10 12:48:55 +0200

    tailwind colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pup: {
+          50: "#EBEFFF",
+          100: "#AFB3FF",
+          200: "#656ED3",
+        },
+        bla: {
+          100: "#323232",
+          200: "#181818",
+          300: "#121212",
+        },
+      },
+    },
   },
   // eslint-disable-next-line no-undef
   plugins: [require("daisyui")],
